d0516e14ad15e34915de8b740d854cb115d56c68
SlienceJoker <2036736521@qq.com>
2020-09-12 21:29:14 +0800

    Second

---
+++ src/while循环.java
@@ -0,0 +1,15 @@
+public class while循环 {
+    public static void main(String[] args) {
+
+        int i = 1; // 代表 1 - 5 之间的数字
+
+        // 当变量小于等于 5 时执行循环
+        while (i<=5) {
+
+            // 输出变量的值，并且对变量加 1，以便于进行下次循环条件判断
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
