0807bbfad3334f93c2f689b892d7e301876eb9a6
SlienceJoker <2036736521@qq.com>
2020-09-14 20:09:37 +0800

    Java入门第一课学习完毕

---
+++ src/Demo03.java
@@ -0,0 +1,11 @@
+public class Demo03 {
+    public static void main(String[] args) {
+        String[] subjects = new String[5];
+        subjects[0]="Oracle";
+        subjects[1]="PHP";
+        subjects[2]="Linux";
+        subjects[3]="Java";
+        subjects[4]="HTML";
+        System.out.println("数组中第四个科目为："+subjects[3]);
+    }
+}
